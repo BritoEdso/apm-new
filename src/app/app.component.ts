1c5c2edb39a82a5e43c4738dfca19b26862553e4
Edson Brito <britoedso@gmail.com>
2023-08-07 15:45:07 -0400

    working with angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+/*
+  selector: defines the name of the component as a derective when used in HTML
+  template: defines the HTML code used in the derective
+*/
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'pm-root', 
+  template: ` 
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <pm-products></pm-products>
+  </div>
+  ` 
 })
+
 export class AppComponent {
-  title = 'apm-new';
+  pageTitle: string = 'Acme Product Management'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './products/product-list.component';
+import { FormsModule } from '@angular/forms';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe], //components must be declared here
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent], //component that is run on start
 })
-export class AppModule { }
+export class AppModule {}
